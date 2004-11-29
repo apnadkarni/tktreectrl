a23cedb4e18964f1ccb673ba4298b3a240ddb1ae
Jeff Hobbs <jeffh@ActiveState.com>
2004-11-29 20:34:40 +0000

    correct Tk defined version check

--- generic/tkTreeCtrl.c
+++ generic/tkTreeCtrl.c
@@ -16,15 +16,14 @@
  * TIP #116 altered Tk_PhotoPutBlock API to add interp arg.
  * We need to remove that for compiling with 8.4.
  */
-#if ((TK_MAJOR_VERSION > 8) || \
-	((TK_MAJOR_VERSION == 8) && (TK_MINOR_VERSION > 4)))
-#define TK_PHOTOPUTBLOCK	Tk_PhotoPutBlock
-#define TK_PHOTOPUTZOOMEDBLOCK	Tk_PhotoPutZoomedBlock
-#else
+#if (TK_MAJOR_VERSION == 8) && (TK_MINOR_VERSION < 5)
 #define TK_PHOTOPUTBLOCK(interp, hdl, blk, x, y, w, cr) \
 		Tk_PhotoPutBlock(hdl, blk, x, y, w, cr)
 #define TK_PHOTOPUTZOOMEDBLOCK(interp, hdl, blk, x, y, w, zx, zy, sx, sy, cr) \
 		Tk_PhotoPutZoomedBlock(hdl, blk, x, y, w, zx, zy, sx, sy, cr)
+#else
+#define TK_PHOTOPUTBLOCK	Tk_PhotoPutBlock
+#define TK_PHOTOPUTZOOMEDBLOCK	Tk_PhotoPutZoomedBlock
 #endif
 
 static CONST char *bgModeST[] = {
