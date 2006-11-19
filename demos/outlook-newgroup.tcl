bb414ce26ed2faa346b1c050eacbf09249c0aacf
Tim Baker <treectrl@users.sourceforge.net>
2006-11-19 00:55:14 +0000

    Use updated [selection get] syntax.

--- demos/outlook-newgroup.tcl
+++ demos/outlook-newgroup.tcl
@@ -168,7 +168,7 @@ proc DemoOutlookNewsgroup {} {
 			if {[info exists Message(afterId)]} {
 				after cancel $Message(afterId)
 			}
-			set Message(afterId,item) [lindex [%T selection get] 0]
+			set Message(afterId,item) [%T selection get 0]
 			set Message(afterId) [after 500 MessageReadDelayed]
 		}
 	}
@@ -358,7 +358,7 @@ proc DemoOutlookNewsgroup_2 {} {
 
 	$T notify bind $T <Selection> {
 		if {[%T selection count] == 1} {
-			set I [lindex [%T selection get] 0]
+			set I [%T selection get 0]
 			if {!$Message(read,$I)} {
 				if {[%T item isopen $I] || ![AnyUnreadDescendants %T $I]} {
 					# unread ->read
