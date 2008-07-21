806e03194e5d91626b23eed6b68e902b996c26bd
Tim Baker <treectrl@users.sourceforge.net>
2008-07-21 18:36:41 +0000

    The <Left> and <Right> arrow keys expand/collapse the current item if -orient=vertical and -wrap={}.

--- library/treectrl.tcl
+++ library/treectrl.tcl
@@ -53,7 +53,11 @@ bind TreeCtrl <Shift-KeyPress-Down> {
     TreeCtrl::Extend %W below
 }
 bind TreeCtrl <KeyPress-Left> {
-    TreeCtrl::SetActiveItem %W [TreeCtrl::LeftRight %W active -1]
+    if {[%W cget -orient] eq "vertical" && [%W cget -wrap] eq ""} {
+	%W item collapse [%W item id active]
+    } else {
+	TreeCtrl::SetActiveItem %W [TreeCtrl::LeftRight %W active -1]
+    }
 }
 bind TreeCtrl <Shift-KeyPress-Left> {
     TreeCtrl::Extend %W left
@@ -62,7 +66,11 @@ bind TreeCtrl <Control-KeyPress-Left> {
     %W xview scroll -1 pages
 }
 bind TreeCtrl <KeyPress-Right> {
-    TreeCtrl::SetActiveItem %W [TreeCtrl::LeftRight %W active 1]
+    if {[%W cget -orient] eq "vertical" && [%W cget -wrap] eq ""} {
+	%W item expand [%W item id active]
+    } else {
+	TreeCtrl::SetActiveItem %W [TreeCtrl::LeftRight %W active 1]
+    }
 }
 bind TreeCtrl <Shift-KeyPress-Right> {
     TreeCtrl::Extend %W right
